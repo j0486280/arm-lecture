44ae4098747fa87d2fd7e4e1761cec311fd1df0d
ray <j0486280@hotmail.com>
2015-10-01 20:40:56 +0800

    Edit c code for compute execute time and output result in txt file.

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,9 +7,20 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
+  
+  int start, finish;
+  double duration;
+  FILE *outfile;
 
   scanf("%d",&number);
+  start = clock();
   result = fibonacci(number);   
+  finish = clock();
+  duration = (double)(finish - start)/CLOCKS_PER_SEC;
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  
+  outfile = fopen("result.txt","a");  
+  fprintf(outfile, "%d\t%f\n",number, duration);
+  fclose(outfile); 
 }
 
